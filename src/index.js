e2bebeb3388f884fb40c950c1fec726fca2ad0af
sanux <sabinnewar6@gmail.com>
2025-10-29 08:57:13 +0545

    a

--- src/index.js
+++ src/index.js
@@ -1,99 +1,109 @@
 
 import './styles.css';
-import {TodoList,Todos,ProjectList,Projects} from './class/project';
+import { TodoList, Todos, ProjectList, Projects } from './class/project';
 // import dropDown from './assets/dropdown.svg';
 
 
-const addTask=document.querySelector('.newTask');
-const showProject=document.querySelector('.optionToShow');
-const projectHidden=document.querySelector('.project-hidden');
-const dialog=document.querySelector('#dialogBox');
-const dialogClose=document.querySelector('.dialogClose');
-const sideBar=document.querySelector('.sidebar-content');
-const slide=document.querySelector('.magic');
-const container=document.querySelector('.container');
-const projectAdd=document.querySelector('.projectAdd');
-const projectClose=document.querySelector('.projectClose');
-const projectDialog=document.querySelector('#projectDialog');
-const projectInput=document.querySelector('.projectInput');
-const title=document.querySelector('.dynamic-tittle');
-const firstContent=document.querySelector('.first-content');
-const addTodoTask=document.querySelector('.addNewTask');
+const addTask = document.querySelector('.newTask');
+const showProject = document.querySelector('.optionToShow');
+const projectHidden = document.querySelector('.project-hidden');
+const dialog = document.querySelector('#dialogBox');
+const dialogClose = document.querySelector('.dialogClose');
+const sideBar = document.querySelector('.sidebar-content');
+const slide = document.querySelector('.magic');
+const container = document.querySelector('.container');
+const projectAdd = document.querySelector('.projectAdd');
+const projectClose = document.querySelector('.projectClose');
+const projectDialog = document.querySelector('#projectDialog');
+const projectInput = document.querySelector('.projectInput');
+const title = document.querySelector('.dynamic-tittle');
+const firstContent = document.querySelector('.first-content');
+const addTodoTask = document.querySelector('.addNewTask');
 // const secondContent=document.querySelector('.second-content');
 
-addTodoTask.addEventListener('click',()=>{
+let projectArray = [];
+let projectList = new ProjectList();
+let todoList = new TodoList(projectList);
+
+addTodoTask.addEventListener('click', () => {
     dialog.showModal();
 })
 
 // use of event delegant 
-firstContent.addEventListener('click',(event)=>{
-    if(event.target.tagName==='P'){
-        title.textContent="";
-        const newH2=document.createElement('H2');
-        newH2.textContent=event.target.textContent;
+firstContent.addEventListener('click', (event) => {
+    if (event.target.tagName === 'P') {
+        title.textContent = "";
+        const newH2 = document.createElement('H2');
+        newH2.textContent = event.target.textContent;
         title.appendChild(newH2);
         // console.log(event.target.textContent);
     }
 })
 
-projectHidden.addEventListener('click',(event)=>{
-      if(event.target && event.target.tagName === 'H4'){
-         title.textContent="";
-        const newH2=document.createElement('H2');
-        newH2.textContent=event.target.textContent;
+projectHidden.addEventListener('click', (event) => {
+    if (event.target && event.target.tagName === 'H4') {
+        title.textContent = "";
+        const newH2 = document.createElement('H2');
+        newH2.textContent = event.target.textContent;
         title.appendChild(newH2);
         console.log(newH2);
     }
 })
 
-slide.addEventListener('click',()=>{
+slide.addEventListener('click', () => {
     sideBar.classList.toggle('sidebar-content-active');
-   container.classList.toggle('sidebar-hidden');
+    container.classList.toggle('sidebar-hidden');
 })
 
-dialogClose.addEventListener('click',()=>{
+dialogClose.addEventListener('click', () => {
     dialog.close();
 })
 
-projectClose.addEventListener('click',()=>{
+projectClose.addEventListener('click', () => {
     projectDialog.close();
 })
-projectAdd.addEventListener('click',()=>{
-    if(projectInput.value==''){
+
+projectAdd.addEventListener('click', () => {
+    if (projectInput.value == '') {
         alert("Enter some project");
-    }else{
-       const newDiv=document.createElement('div');
-       const title=document.createElement("H4");
-       title.textContent=projectInput.value;
-       newDiv.appendChild(title);
-       projectHidden.appendChild(newDiv);
-       projectDialog.close();
-       projectInput.value="";
+    } else {
+        const newDiv = document.createElement('div');
+        const title = document.createElement("H4");
+        title.textContent = projectInput.value;
+        newDiv.appendChild(title);
+        projectHidden.appendChild(newDiv);
+
+        const project = projectList.addProject(projectInput.value);
+        projectArray.push(project);
+        console.log(projectArray);
+
+        projectDialog.close();
+        projectInput.value = "";
     }
 
 })
 
-addTask.addEventListener('click',()=>{
-   projectDialog.showModal();
+addTask.addEventListener('click', () => {
+    projectDialog.showModal();
 })
 
-function magic(){
+function magic() {
     projectHidden.classList.toggle("active");
 }
 
-showProject.addEventListener('click',magic);
+showProject.addEventListener('click', magic);
 
 
 
 
 
 
-let projectList=new ProjectList();
-const p1=projectList.addProject("sunday");
-const p2=projectList.addProject("monday");
+// let projectList=new ProjectList();
+// const p1=projectList.addProject("sunday");
+// const p2=projectList.addProject("monday");
 
-let todoList=new TodoList(projectList);
-todoList.addTodo(p1.id,"sunday task","wake up and do exercise","monday","high");
-todoList.addTodo(p2.id,"monday task","do exercise","sunday","high");
+// let todoList=new TodoList(projectList);
+// todoList.addTodo(p1.id,"sunday task","wake up and do exercise","monday","high");
+// todoList.addTodo(p2.id,"monday task","do exercise","sunday","high");
 
-console.log(projectList);
+// console.log(projectList);
